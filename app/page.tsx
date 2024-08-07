b1c78cc810f0f53d8d8f477d4232fb69624507bc
noranaziz <noranaziz18@icloud.com>
2024-08-07 01:00:14 -0400

    added button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p>Hello Notion Clone :D</p>
+    <div>
+      <Button>
+        Click Me :D
+      </Button>
+    </div>
   );
 }
