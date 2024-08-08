695921512d2b00798e2e7621b508e77bf5692fc4
noranaziz <noranaziz18@icloud.com>
2024-08-08 01:45:25 -0400

    created footer

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <div className = "flex items-center w-full p-6 bg-background z-50">
+            footer
+        </div>
+    );
+}
+ 
+export default Footer;
